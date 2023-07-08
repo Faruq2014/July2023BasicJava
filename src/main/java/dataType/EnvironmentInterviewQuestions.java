1fc2ec38357e1f9b7a5ed72a514cee15ee206627
Mohammed Molla <mjmolla@gmail.com>
2023-07-08 13:58:47 -0400

    init commit

---
+++ src/main/java/dataType/EnvironmentInterviewQuestions.java
@@ -0,0 +1,47 @@
+package dataType;
+
+public class EnvironmentInterviewQuestions {
+
+	public static void main(String[] args) {
+		// how do comment out single line?
+       // how do comment out multiple  line?
+		
+		/*
+		 * gww
+		 * ghhjksd
+		 * jhsjkhsjks
+		 */
+		// what is syntax?
+		System.out.println("heelloo JAVA"); System.out.println("haha");
+		System.out.println("hehe");
+		//A complete sentence in java that produce output
+		//how do you set up maven project
+		/*
+		 * file-->new--maven project-->select simple project-->
+		 * artifact Id-->group ID -->click finish 
+		 */
+		//java syntax execute top to bottom and then left to right
+		//what is encapsulation
+		//how do you reset prospective
+		//what is pom.xml file
+		/*
+		 * it is settings for the project
+		 * 
+		 */
+		//what is properties
+		//Configuration for different dependencies
+		// such waht java version should i have
+		
+		// what is artifact ID
+		// is the project name such as July2023BasicJava
+		//what is group ID
+		// it is the package name such July2023BasicJava
+		//group ID and Artifact ID can be different
+		//what is JRE
+		//what is JVM
+		//what is JVC
+		//What is JDK
+	
+	}
+
+}
